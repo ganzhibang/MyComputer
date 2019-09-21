2150f871ef0f360e2418962fc4277981ed0cc785
2017yd <2017yd@lzzydeiMac.local>
2019-09-21 12:03:46 +0800

    Initial Commit

---
+++ MyComputer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyComputer
+//
+//  Created by 2017yd on 2019/9/21.
+//  Copyright © 2019 2017yd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
